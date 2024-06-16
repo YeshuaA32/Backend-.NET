4d6df4fe14bdd92c166d568b4bbaf2d2fd16e297
Benja Alonzo <aalonzom32@gmail.com>
2024-06-15 20:32:35 -0600

    agregado el GenericRepository

---
+++ SistemaVenta.DAL/Repositorios/GenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SistemaVenta.DAL.Repositorios.Contrato;
+using SistemaVenta.DAL.DBContext;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace SistemaVenta.DAL.Repositorios
+{
+    public class GenericRepository
+    {
+    }
+}
